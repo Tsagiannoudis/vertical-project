8e337fe41bc492518c4121c6d98f100987413400
Ioakeim Tsagiannoudis <i.tsagiannoudis@gmail.com>
2025-08-27 16:03:23 +0300

    css text fixed

--- src/components/home/WhatWeOfferToYou.tsx
+++ src/components/home/WhatWeOfferToYou.tsx
@@ -1,17 +1,19 @@
 const WhatWeOfferToYou = () => {
   return (
-    <section className="w-full mx-auto p-8 bg-[#F3F3F3]">
-      <div className="container mx-auto text-black">
+    <section className="w-full mx-auto p-4 sm:p-8 bg-[#F3F3F3]">
+      <div className="container mx-auto text-black text-center">
         <h2 className="text-3xl font-bold text-center">Τι σας προσφέρουμε</h2>
-        <p className="text-lg mt-4 text-left">
-          Στόχος μας είναι να δημιουργούμε δασκάλους με γνώσεις, αυτοπεποίθηση
-          και έμπνευση.
-        </p>
-        <p className="text-lg mt-4 text-justify">
-          Προσφέρουμε εξειδικευμένες εκπαιδεύσεις στους τομείς της Aerial Yoga,
-          της Yoga, Handstands και Thai Yoga Massage, βασισμένες σε χρόνια
-          εμπειρίας και διεθνή πρότυπα.
-        </p>
+        <div className="max-w-4xl mx-auto mt-4 space-y-4">
+          <p className="text-lg">
+            Στόχος μας είναι να δημιουργούμε δασκάλους με γνώσεις, αυτοπεποίθηση
+            και έμπνευση.
+          </p>
+          <p className="text-lg">
+            Προσφέρουμε εξειδικευμένες εκπαιδεύσεις στους τομείς της Aerial Yoga,
+            της Yoga, Handstands και Thai Yoga Massage, βασισμένες σε χρόνια
+            εμπειρίας και διεθνή πρότυπα.
+          </p>
+        </div>
       </div>
     </section>
   );
