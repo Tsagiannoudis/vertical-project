88b2813e9228527dfd1d1cc6f7e7e9e746204ae8
Tsagiannoudis <i.tsagiannoudis@gmail.com>
2025-08-29 21:22:27 +0300

    css home update

--- src/components/home/WhatWeOfferToYou.tsx
+++ src/components/home/WhatWeOfferToYou.tsx
@@ -45,7 +45,7 @@ const WhatWeOfferToYou = () => {
             {lessons.map((lesson, index) => (
               <li
                 key={index}
-                className="bg-[#D4CBC2] p-8 rounded-2xl shadow-sm text-center transition-transform duration-300 hover:scale-105 hover:shadow-md"
+                className="bg-[#a1ff81] p-8 rounded-2xl shadow-sm text-center transition-transform duration-300 hover:scale-105 hover:shadow-md"
               >
                 <h3 className="text-2xl mb-4 font-semibold text-gray-800">
                   {lesson.title}
--- src/components/home/WhyToBecomeATeacher.tsx
+++ src/components/home/WhyToBecomeATeacher.tsx
@@ -32,7 +32,7 @@ const CheckCircleIcon = (props: React.SVGProps<SVGSVGElement>) => (
 
 const WhyToBecomeATeacher = () => {
   return (
-    <section className="w-full mx-auto py-16 sm:py-24 bg-[#D4CBC2]">
+    <section className="w-full mx-auto py-16 sm:py-24 bg-[#F3F3F3]">
       <div className="container mx-auto px-4 text-gray-800">
         <h2 className="text-3xl md:text-4xl mt-6 mb-10 font-bold text-center">
           Γιατί να γίνεις Vertical Teacher
@@ -46,7 +46,7 @@ const WhyToBecomeATeacher = () => {
           <ul className="grid grid-cols-1 md:grid-cols-2 gap-x-16 gap-y-10">
             {benefits.map((benefit, index) => (
               <li key={index} className="flex items-center">
-                <div className="flex-shrink-0 h-12 w-12 rounded-full bg-white/50 flex items-center justify-center ring-2 ring-white/20">
+                <div className="flex-shrink-0 h-12 w-12 rounded-full bg-[#a1ff81] flex items-center justify-center ring-2 ring-white/20">
                   <CheckCircleIcon className="w-7 h-7 text-gray-700" />
                 </div>
                 <p className="ml-5 text-lg font-medium">{benefit.text}</p>
