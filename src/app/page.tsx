88c3fbabfc9081ed5204b8ea93e3b7c436d5e3a0
Tsagiannoudis <i.tsagiannoudis@gmail.com>
2025-09-15 21:20:35 +0300

    remove blog section from index

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,19 +31,6 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="w-full mx-auto py-16 sm:py-24 bg-[#fff]">
-        <div className="container mx-auto text-center text-black max-w-4xl"> 
-          <h2 className="text-3xl md:text-4xl font-bold">Μείνετε Ενημερωμένοι</h2>
-          <p className="mt-4 text-lg text-gray-700">
-            Ακολουθήστε το blog μας για τα τελευταία νέα, άρθρα και ενημερώσεις από τον κόσμο της aerial yoga και της εκπαίδευσης.
-          </p>
-          <div className="mt-8">
-            <Link href="/blog" className="bg-transparent border-2 border-black text-black font-semibold py-3 px-8 rounded-ss-2xl rounded-br-2xl hover:bg-[#F2E94E] hover:text-black transition-colors duration-300 transform hover:scale-105">
-              Επισκεφθείτε το Blog
-            </Link>
-          </div>
-        </div>
-      </section>
     </main>
   );
 }
