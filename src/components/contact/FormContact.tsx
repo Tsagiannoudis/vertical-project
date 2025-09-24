9c863783de3fafa98f1558b6b2c9e6579b02b1c9
Tsagiannoudis <i.tsagiannoudis@gmail.com>
2025-09-24 22:35:11 +0300

    add text contactform

--- src/components/contact/FormContact.tsx
+++ src/components/contact/FormContact.tsx
@@ -70,7 +70,6 @@ export default function FormContact() {
             <div className="container mx-auto max-w-5xl mb-8 pt-10">
                 <h2 className="text-3xl font-semibold text-black">Επικοινωνήστε μαζί μας</h2>
                 <p className="text-gray-600 mt-2 mb-4">Μπορείτε να μας βρείτε μέσω των Social Media ή να συμπληρώσετε την παρακάτω φόρμα επικοινωνίας και θα επικοινωνήσουμε μαζί σας σύντομα.</p>
-                <p>παρακάτω θα βρείτε τους τρόπους επικοινωνίας μας:</p>
                 <div className="space-y-2">
                     <p><strong>Email:</strong> <a href="mailto:info@verticalproject.gr" className="text-gray-600 hover:underline">info@verticalproject.gr</a></p>
                     <p><strong>Phone:</strong> <a href="tel:+306945454545" className="text-gray-600 hover:underline">-</a></p>
