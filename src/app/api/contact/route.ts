4cc3966302354075d3b391538d3b0af6e279d7be
Ioakeim Tsagiannoudis <i.tsagiannoudis@gmail.com>
2025-10-02 14:54:20 +0300

    update mail route sender

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -21,8 +21,9 @@ export async function POST(request: Request) {
 
     // Στέλνουμε το email χρησιμοποιώντας το Resend
     const { data, error } = await resend.emails.send({
+      from: `Vertical Project <onboarding@resend.dev>`,
       // Αλλάζουμε τον αποστολέα για να χρησιμοποιεί το δικό μας, επιβεβαιωμένο domain
-      from: `Vertical Project <info@verticalproject.gr>`,
+      from: `Vertical Project <contact@verticalproject.gr>`,
       to: [emailTo],
       // Το reply_to επιτρέπει να πατήσεις "Απάντηση" και να απαντήσεις απευθείας στον χρήστη
       replyTo: email,
