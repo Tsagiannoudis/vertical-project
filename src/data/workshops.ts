00eb8e807140ac4248e798632a281607c6603072
Tsagiannoudis <i.tsagiannoudis@gmail.com>
2026-01-08 17:03:32 +0200

    new workshop

--- src/data/workshops.ts
+++ src/data/workshops.ts
@@ -28,7 +28,7 @@ export const workshops = [
         title: 'Acroyoga & Standing Acrobatics Foundations guided by John Karvelis and Eleni Fotiou',
         date: '07 Φεβρουαρίου 2026',
         time: '16:00 - 19:00',
-        location: 'Studio Loopa, Aθήνα',
+        location: 'Loopa.acrobatics&more, Aθήνα',
         shortDescription: 'Ένα σεμινάριο γεμάτο στηρίξεις και ανάστροφες θέσεις, βασισμένο στην αλληλουποστήριξη και τα ασφαλή progressions. Πραγματοποιήται στο Studio Αιώρησις στην Θεσσαλονίκη.',
         image: '/workshops-retreats/assists-in-inversions.webp',
         instructor: 'Γιάννης Καρβέλης & Ελένη Φωτίου',
