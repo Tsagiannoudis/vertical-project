3b44f27164b1af2ba59b1461f8caf4f802c898f6
Tsagiannoudis <i.tsagiannoudis@gmail.com>
2026-02-07 20:28:28 +0200

    css title event fixed for mobile

--- src/components/events/HeroSectionEvents.tsx
+++ src/components/events/HeroSectionEvents.tsx
@@ -4,8 +4,8 @@ const HeroSectionEvents = () => {
   return (
     <section className="relative w-full h-[300px] bg-[#3c3c3c]">
       <div className="container mx-auto max-w-7xl px-4 absolute inset-x-0 bottom-0 mb-4 text-left text-white">
-        <h1 className="text-5xl font-bold">Trainings & Workshops</h1>
-        <p className="text-xl mt-2 text-gray-200">Ανακαλύψτε τα επερχόμενα σεμινάρια και εκπαιδευτικά προγράμματα.</p>
+        <h1 className="text-3xl md:text-5xl font-bold">Trainings & Workshops</h1>
+        <p className="text-lg md:text-xl mt-2 text-gray-200">Ανακαλύψτε τα επερχόμενα σεμινάρια και εκπαιδευτικά προγράμματα.</p>
       </div>
     </section>
   );
